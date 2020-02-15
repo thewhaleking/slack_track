96ac47ac9879707e913cd2a67de154145c6828d6
Benjamin Himes <bhimes@uber.com>
2020-02-14 20:55:22 -0500

    Fixed a refactored naming error in setup.sh, fixed the execution of the Python binary.

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ ! -d "$SCRIPT_PATH/venv" ];
   then python3 -m venv "$SCRIPT_PATH/venv";
 fi;
-PY_BIN="$SCRIPT_PATH/venv/bin/python";
-"$PY -m pip install -U pip";
-"$PY -m pip install -r requirements.txt";
-"$PY setup.py";
+PY="$SCRIPT_PATH/venv/bin/python";
+"$PY" -m pip install -U pip;
+"$PY" -m pip install -r requirements.txt;
+"$PY" setup.py;
 
