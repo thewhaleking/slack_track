5e18333a70cca50c902ebee2864653d492866acb
Benjamin Himes <bhimes@uber.com>
2020-05-08 14:07:04 -0400

    Add call to `python -m ensurepip`, as the user may not have pip3 installed.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ if [ ! -d "$SCRIPT_PATH/venv" ];
   then python3 -m venv "$SCRIPT_PATH/venv";
 fi;
 PY="$SCRIPT_PATH/venv/bin/python";
+"$PY" -m ensurepip;
 "$PY" -m pip install -U pip;
 "$PY" -m pip install -r requirements.txt;
 "$PY" install.py;
